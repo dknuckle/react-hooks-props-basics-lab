bbd657a49c5014ff9b001599d198bd56623d78f9
Dalton Knuckles <djck244@gmail.com>
2023-05-27 08:38:43 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,28 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+import Links from "./Links";
 console.log(user);
+
+function PropsForHome(user) {
+  return <Home city={user.city} color={user.color} name={user.name} />;
+}
+
+function PropsForAbout(user) {
+  return <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />;
+}
+
+/* function PropsForLinks(user) {
+  return <Links github={user.links.github} linkedin={user.links.linkedin} />;
+}
+PropsForLinks(); */
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      {PropsForHome(user)}
+      {PropsForAbout(user)}
     </div>
   );
 }
